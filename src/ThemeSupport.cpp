3325caaaf8a6a9d45b4327544b006f2ab2db3fd4
Adrian Carpenter <adrian@fizzyade.com>
2021-05-12 01:43:37 +0100

    chore: refactoring
    - refactoring of some class names and the renaming of includes to include
    - the include folder now contains only headers that should be public, and those headers are without an extension.

--- src/ThemeSupport.cpp
+++ src/ThemeSupport.cpp
@@ -21,7 +21,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "includes/ThemeSupport.h"
+#include "ThemeSupport.h"
 
 #include <QApplication>
 #include <QFile>
